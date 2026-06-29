3747dc1bd39a1ec6b9f8a1d1eacef6b9ccba93ac
eduardo <eduardoetolaba@gmail.com>
2026-06-29 00:01:26 -0300

    asignacion de roles, verificacion de usuario logeado, y tener acceso a acciones de acuerdo a permisos

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Panel de control tienda de ropa
 
-*** Este proyecto fue realizado con
+Este proyecto fue realizado con Django
 
 
 ## Pasos a seguir para levantar el servidor en tu ordenar.
@@ -38,4 +38,37 @@ Como en el proyecto actual si esta copiada la base de datos no es necesario real
 
 ```
 python manage.py runserver
+```
+
+Abrir en el lavegador la siguiente URL:
+
+```http://127.0.0.1:8000/login/```
+
+##  Objetivo del proyecto
+
+Mostrar una panel de gestion de stock de productos en una tienda de ropas. dependiendo el rol de quien se logea pueder realizar diferentes acciones.
+
+hay tres roles disponibles:
+
+* admin: puede agregar, eliminar, cambiar y consultar los productos.
+* operador: unicamente puede cambiar un producto ya sea el stock o el nombre, pero no puede eliminar ni añadir otro producto.
+* consultor: solo puede realizar consulta de productos pero no puede realizar ninguna otra accion.
+
+hay usuarios predefinidos y la password es la misma para todos.
+```
+Usernames
+
+admin:{
+    JuanPE
+}
+Consultor:{
+    RamonR
+    Ferchu
+}
+Operador: {
+    EduardoT
+}
+
+passwords:awds1234
+
 ```
--- productos/views.py
+++ productos/views.py
@@ -4,9 +4,10 @@ from .models import Producto
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 
-# aqui listo los pruductos que se encuentran en la base de datos, y los muestro en la plantilla listar_pructos.html
+# aqui listo los pruductos que se encuentran en la base de datos, y los muestro en la plantilla listar_productos.html
 class ProductoListView(ListView):
     model = Producto
+    permission_required = 'productos.view_producto' #agregamos los permisos segun roles
     template_name = 'productos/listar_productos.html'
 
 # Vista para crear un producto
@@ -15,7 +16,7 @@ class ProductoCreateView(LoginRequiredMixin, CreateView):
     fields = ['nombre', 'descripcion', 'precio','stock']
     success_url = reverse_lazy('listar_productos')
     template_name = 'productos/crear_producto.html'
-
+    permission_required = 'productos.add_producto'
     def form_valid(self, form):
         form.instance.usuario = self.request.user # Asigna el producto al usuario logueado
         return super().form_valid(form)
@@ -24,12 +25,14 @@ class ProductoCreateView(LoginRequiredMixin, CreateView):
 class ProductoUpdateView(LoginRequiredMixin, UpdateView):
     model = Producto
     fields = ['nombre', 'descripcion', 'precio', 'stock']
+    permission_required = 'productos.change_producto'
     success_url = reverse_lazy('listar_productos')
     template_name = 'productos/crear_producto.html'
 
 # Vista para borrar un producto
 class ProductoDeleteView(LoginRequiredMixin, DeleteView):
     model = Producto
+    permission_required = 'productos.delete_producto'
     success_url = reverse_lazy('listar_productos')
     template_name = 'productos/eliminar_producto.html'
 
